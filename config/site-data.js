440d34052ebdf439c77b2bf255cc1df5b51d8e9a
jcx0110 <jiangchenxi0110@gmail.com>
2026-09-24 15:15:38 +0800

    Update CoRL BibTeX metadata

--- config/site-data.js
+++ config/site-data.js
@@ -186,12 +186,12 @@ window.SITE_DATA = {
   ],
 
   bibtex: `@inproceedings{guo2026chameleon,
-  title     = {Chameleon: Control-Indexed Prospective Memory for Visuomotor Manipulation},
-  author    = {Guo, Xinying and Jiang, Chenxi and Kim, Hyun Bin
-               and Han, Yuhang and Sun, Ying and Xiao, Yang
-               and Yang, Jianfei},
-  booktitle = {Conference on Robot Learning (CoRL)},
-  year      = {2026},
-  url       = {https://openreview.net/forum?id=TTJ3uxLcxL}
+  title     = {{Chameleon}: Control-Indexed Prospective Memory for Visuomotor Manipulation},
+  author    = {Guo, Xinying and Jiang, Chenxi and Kim, Hyun Bin and Han, Yuhang
+               and Sun, Ying and Xiao, Yang and Yang, Jianfei},
+  booktitle = {Proceedings of The 10th Conference on Robot Learning},
+  series    = {Proceedings of Machine Learning Research},
+  publisher = {PMLR},
+  year      = {2026}
 }`
 };
